5ca6c19e7f63263a2630d59497f4b0870a1acacc
IanMezza <ian.mezza@gmail.com>
2015-01-14 17:09:07 -0600

    Se agregan credenciales de tuiter

--- config.js
+++ config.js
@@ -3,8 +3,15 @@
  */
 module.exports = {
 	database : {
-		host: 'Database_host',	//'localhost'
-		user: 'Database_user',	//'root'
-		password: 'password'	//''
+		host	: 'localhost',
+		user	: 'root',
+		password: ''
+	}
+
+	twitter : {
+		consumer_key		: 'consumer_key',
+		consumer_secret		: 'consumer_secret',
+		access_token_key	: 'access_token_key',
+		access_token_secret	: 'access_token_secret'
 	}
 }
